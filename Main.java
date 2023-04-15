ab5fc83d0e913331549eabb7ac4e7c93df1277f9
jack1915 <109028048+jack1915@users.noreply.github.com>
2023-04-15 18:41:00 +0500

    src

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        RedBlackTree node = new RedBlackTree();
+        RedBlackTree<Integer> node = new RedBlackTree();
 
         node.insert(1);
         node.inorder();
